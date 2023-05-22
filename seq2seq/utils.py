e8652f33bd48f9189b861bd55eaaf96e15dbd6b5
Анна <a.marshalova@g.nsu.ru>
2023-05-22 11:35:30 +0700

    bart model added

--- seq2seq/utils.py
+++ seq2seq/utils.py
@@ -1,7 +1,7 @@
 import gc
 import torch
 import numpy as np
-from transformers import T5ForConditionalGeneration, MT5ForConditionalGeneration
+from transformers import T5ForConditionalGeneration, MT5ForConditionalGeneration, BartForConditionalGeneration
 
 BATCH_SIZE = 32
 RANDOM_STATE = 42
@@ -10,6 +10,7 @@ MODEL_CONFIG = {
     'rut5-base':{'pretrained_model_name':'sberbank-ai/ruT5-base', 'model_class':T5ForConditionalGeneration},
     'mt5-small':{'pretrained_model_name':'google/mt5-small', 'model_class':MT5ForConditionalGeneration},
     'paraphraser': {'pretrained_model_name':'cointegrated/rut5-base-paraphraser', 'model_class':T5ForConditionalGeneration},
+    'bart': {'pretrained_model_name':'sn4kebyt3/ru-bart-large', 'model_class':BartForConditionalGeneration}
 }
 
 def cleanup():
