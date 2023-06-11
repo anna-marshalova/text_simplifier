944da7b27060e8197f1f89451cd0a52c10e336ab
Анна Маршалова <78635473+anna-marshalova@users.noreply.github.com>
2023-06-11 22:18:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,1 @@
-from utils import MODEL_NAME
-from app.simplifier import Simplifier
 
-simplifier = Simplifier(MODEL_NAME)
-text = '14 декабря 1944 года рабочий посёлок Ички был переименован в рабочий посёлок Советский, после чего поселковый совет стал называться Советским.'
-print(simplifier.simplify(text))
